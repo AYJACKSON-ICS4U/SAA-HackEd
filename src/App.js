e659ffe608d4dbbd6602ab232963e52287799dcc
Anoushka Jawale <anoushkajawale@gmail.com>
2019-12-09 14:57:07 -0500

    Convert to react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import background from  './background.png';
 
 function App() {
+  function handleClick() {
+    console.log("Clicked!")
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className = "header">
+        <h1 className="text-center">FlashTime</h1>
+      </div>
+      <img src = {background} ></img>
+      <div className = "text-center">
     </div>
+    <div className = "form-container">
+      <div className = "font">
+      <h3 className = "text-center" >SIGN UP</h3>
+    </div>
+    <div class = "form">
+        <form>
+            <div className="form-group">
+                <div className = "font">
+              <label for="email"><h4>Email address</h4></label>
+              </div>
+              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"></input>
+            </div>
+            <div className="form-group">
+                <div className = "font">
+              <label for="password"><h4>Password</h4></label>
+              </div>
+              <input type="password" className="form-control" id="exampleInputPassword1" placeholder="Password"></input>
+            </div>
+            <a href="homepage.html" className="btn btn-info" type = "button" role="submit" aria-pressed="true">Submit</a>
+            <a href="login.html" onClick={handleClick()}className="btn btn-info" type = "button" role="submit" aria-pressed="true">Login</a>
+          </form>
+       </div>
+      </div>
+    </div>
+  
+    
   );
 }
 
