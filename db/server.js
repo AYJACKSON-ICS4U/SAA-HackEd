c05843f534de0cf4d34f798d0a0a1e99ed28e276
sabrinabutton <43886209+sabrinabutton@users.noreply.github.com>
2019-12-10 21:25:37 -0500

    Created an Express API to call from client.

---
+++ db/server.js
@@ -0,0 +1,57 @@
+//requirements
+const app = require("express")();
+const http = require("http").createServer(app);
+const cors = require("cors");
+const db = require("./db-comm.js");
+
+//enable cors
+app.use(cors());
+
+//api calls for each db communication function
+app.get("/api/createuser/", (req, res) => {
+    db.createUser(req.query.username,req.query.email,req.query.password);
+    res.send("Created a new user.");
+});
+
+app.get("/api/createset/", (req, res) => {
+    db.createSet(req.query.username,req.query.email,req.query.password);
+    res.send("Created a new set.");
+});
+
+app.get("/api/createcard/", (req, res) => {
+    db.createCard(req.query.definition,req.query.term,req.query.set,req.query.owner);
+    res.send("Created a new card.");
+});
+
+app.get("/api/deleteuser/", (req, res) => {
+    db.deleteUser(req.query.username);
+    res.send("Deleted a user.");
+});
+
+app.get("/api/deleteset/", (req, res) => {
+    db.deleteSet(req.query.owner, req.query.set);
+    res.send("Deleted a set.");
+});
+
+app.get("/api/deletecard/", (req, res) => {
+    db.deleteCard(req.query.owner, req.query.set, req.query.card);
+    res.send("Deleted a card.");
+});
+
+app.get("/api/verifylogin/", (req, res) => {
+    var status = db.verifyLogin(req.query.username, req.query.password);
+    res.send(status);
+});
+
+app.get("/api/getuserdata/", (req, res) => {
+    var status = db.getUserData(req.query.username);
+    res.send(status);
+});
+
+//set port to 3001 (TODO: set to any available port)
+var port = 3006;
+
+//listen on the port provided
+http.listen(port, () => {
+  console.log("Listening on port:", port);
+});
