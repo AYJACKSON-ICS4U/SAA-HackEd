551f5a8506c5f49110e48d8cb5238296fd931a3b
Sabrina Button <43886209+sabrinabutton@users.noreply.github.com>
2019-12-14 23:10:12 -0500

    Update app.js

--- db/app.js
+++ db/app.js
@@ -15,7 +15,7 @@ app.get("/api/createuser/", async (req, res) => {
             res.send("Created a new user.");
     }
     else{
-        res.send(0);
+        res.send("0");
     }
 });
 
@@ -54,7 +54,7 @@ app.get("/api/getuserdata/", async (req, res) => {
     var status = await db.getUserData(req.query.username);
     //send user info back to frontend
     if(status == null){
-        res.send(0);
+        res.send("0");
     }
   res.send(status)
 });
